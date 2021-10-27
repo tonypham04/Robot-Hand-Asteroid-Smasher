38ecc73f9ca0da38ae763311ef96797d539cbf06
Tony Pham <tony.phamx04@gmail.com>
2021-10-27 18:15:10 -0400

    Setup: Add main function with setup for a caption, scaled display, 60fps framerate and graceful termination on clicking 'X' or pressing 'ESC'

--- game.py
+++ game.py
@@ -1,5 +1,23 @@
+import pygame
+import sys
+
 def main():
-    pass
+    """The main method runs when the script is run and houses the game loop and variables for the game."""
+    pygame.init()
+    screen = pygame.display.set_mode((480, 320))
+    gameview = pygame.Surface((int(screen.get_width() / 2), int(screen.get_height() / 2)))
+    pygame.display.set_caption('Robot Hand Asteroid Smasher')
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(60)
+        gameview.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+        scaled_gameview = pygame.transform.scale(gameview, (screen.get_width(), screen.get_height()))
+        screen.blit(scaled_gameview, (0, 0))
+        pygame.display.update()
 
 if __name__ == '__main__':
     main()
