053646a837db3888815e50d172c19d1c760fae1e
Tony Pham <tony.phamx04@gmail.com>
2021-10-28 17:23:11 -0400

    Setup: Add function to load an image and add Player class with logic to update position based on mouse cursor location

--- game.py
+++ game.py
@@ -1,23 +1,69 @@
 import pygame
 import sys
+import os
+
+# Platform independent paths
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+img_dir = os.path.join(main_dir, 'images')
+
+def load_image(img_filename: str, colorkey=None) -> pygame.Surface:
+    """Loads an image from an image file and applies transparent color if applicable. A Surface representation of the image is returned."""
+    # Attempt to load the user specified image
+    img_location = os.path.join(img_dir, img_filename)
+    try:
+        img = pygame.image.load(img_location).convert()
+    except pygame.error as msg:
+        print('Failed to load:', img_filename)
+        raise SystemExit(msg)
+    # Apply color transparency if applicable
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = img.get_at((0, 0))
+        # The pygame.RLEACCEL flag provides better performance on non accelerated displays
+        img.set_colorkey(colorkey, pygame.RLEACCEL)
+    return img
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        """Initialize an instance of the Player class."""
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image('player.png', (255, 255, 255))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        """Updates the player position based on where the mouse cursor is."""
+        pos = pygame.mouse.get_pos()
+        # The actual gameview is 1/2 the screen size so divide the mouse coordinates by 2 to account for the 2x scaling
+        self.rect.center = (pos[0] / 2, pos[1] / 2)
+
 
 def main():
     """The main method runs when the script is run and houses the game loop and variables for the game."""
+    # Initialize game components
     pygame.init()
     screen = pygame.display.set_mode((480, 320))
     gameview = pygame.Surface((int(screen.get_width() / 2), int(screen.get_height() / 2)))
     pygame.display.set_caption('Robot Hand Asteroid Smasher')
+    pygame.mouse.set_visible(False)
     clock = pygame.time.Clock()
+    # Prepare game objects
+    player = Player()
+    sprites_group = pygame.sprite.RenderPlain((player))
     while True:
         clock.tick(60)
-        gameview.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+        sprites_group.update()
+        gameview.fill((0, 0, 0))
+        sprites_group.draw(gameview)
         scaled_gameview = pygame.transform.scale(gameview, (screen.get_width(), screen.get_height()))
         screen.blit(scaled_gameview, (0, 0))
         pygame.display.update()
 
 if __name__ == '__main__':
     main()
+# References
+# Line by Line Chimp (pygame docs, https://www.pygame.org/docs/tut/ChimpLineByLine.html)
+# pygame.Surface.set_colorkey (pygame docs, https://www.pygame.org/docs/ref/surface.html#pygame.Surface.set_colorkey)
